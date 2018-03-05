4f3b608b594810100bec995c45fe6bc5f2b1916d
Vinicius Bussola <vbussola@yahoo.com>
2018-03-05 16:41:32 -0300

    .

--- projeto_fer/site_gula/views.py
+++ projeto_fer/site_gula/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'site/index.html')
 
 def graficos(request):
     return render(request, 'graficos.html')
